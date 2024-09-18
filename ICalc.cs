5ce61621d131aae306542e2e6a45e8bb30f281ba
Arsen Sevastyanov <130586920+MakepeaceUA@users.noreply.github.com>
2024-09-18 21:11:49 +0300

    Add files via upload

---
+++ ICalc.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal interface ICalc
+    {
+        int Less(int valueToCompare);
+        int Greater(int valueToCompare);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Drawing;
+using System.Xml.Linq;
+using static ConsoleApp3.Program;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] num = { 1, 2, 3, 4, 5 };
+            MyArray arr = new MyArray(num);
+
+            arr.Show();
+            arr.Show("Test array.");
+
+            arr.Max();
+            arr.Min();
+            Console.WriteLine($"Arithmetic mean: {arr.Avg()}");
+            Console.WriteLine($"Search: {arr.Search(3)}");
+            Console.WriteLine();
+
+            arr.SortAsc();
+            arr.Show();
+
+            arr.SortDesc();
+            arr.Show();
+
+            arr.SortByParam(true);
+            arr.Show();
+
+            arr.SortByParam(false);
+            arr.Show();
+
+            Console.WriteLine(arr.Less(6));
+            Console.WriteLine(arr.Greater(6));
+
+            arr.ShowEven();
+            arr.ShowOdd();
+
+            Console.WriteLine(arr.CountDistinct());
+            Console.WriteLine(arr.EqualToValue(5));
+
+        }
+    }
+}
